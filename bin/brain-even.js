b01ab8b37e57701361812a26a1ba7d8c222bc3ae
Vitaly <119169683+StoyingV@users.noreply.github.com>
2023-02-27 21:44:05 +0300

    Correct gramma

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -8,8 +8,8 @@ import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
 
-const name = readlineSync.question('May I have you name? ');
-console.log('Hello,', name, '!');
+const name = readlineSync.question('May I have your name?');
+console.log(`Hello, ${name}!`);
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
@@ -39,7 +39,7 @@ while (i < 3) {
 };
 
 if (i === 3) {
-console.log('Congratulations,',name,'!');
+console.log(`Congratulations, ${name}!`);
 };
 
 
