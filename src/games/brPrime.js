12045202e0443c38b4b159a1ddad5289a18e5751
Михаил Джалилов <dzhalilov.mikhail@hexly.ru>
2023-03-13 19:12:20 +0300

    Исправляю ошибки. Разделяю игры, на файлы с игрой и функции для них Brain-prime Brain-progression Игра

--- src/games/brPrime.js
+++ src/games/brPrime.js
@@ -1,40 +1,34 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import { getRandomInt, prime } from '../src/index.js';
+import { getRandomInt, prime } from '../index.js';
 
-console.log('Welcome to the Brain Games!');
-
-const name = readlineSync.question('May I have your name? ');
-console.log('Hello,', name, '!');
-
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-
-let i = 0;
-
-while (i < 3) {
-    let a1 = getRandomInt(100);
-    if ( a1 === 0 ) {
-        a1 = 1;
+const BrPrime = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+    let i = 0;
+    while (i < 3) {
+        let a1 = getRandomInt(100);
+        if ( a1 === 0 ) {
+            a1 = 1;
+        };
+        var mas = prime(a1);
+        console.log('Question:', a1);
+        const name1 = readlineSync.question('Your answer: ');
+            if (name1 === mas) {
+                i +=1;
+                console.log('Correct!');  
+            } else {
+                i = 0;
+                console.log(`'${name1}' is wrong answer ;(. Correct answer was '${mas}'.`);
+                console.log(`Let's try again, ${name}!`);
+                break;
+            }; 
     };
-
-    var mas = prime(a1);
-
-    console.log('Question:', a1);
-
-    const name1 = readlineSync.question('Your answer: ');
-        if (name1 === mas) {
-            i +=1;
-            console.log('Correct!');  
-        } else {
-            i = 0;
-            console.log("'",name1,"' is wrong answer ;(. Correct answer was '",mas,"'.");
-            console.log("Let's try again,",name,'!');
-            break;
-        }; 
-
+    if (i === 3) {
+        console.log(`Congratulations, ${name}!`);
+    };
 };
-
-if (i === 3) {
-    console.log(`Congratulations, ${name}!`);
-};
+export default BrPrime;
--- src/games/brProgression.js
+++ src/games/brProgression.js
@@ -1,44 +1,39 @@
 #!/usr/bin/env node
 
 import readlineSync from 'readline-sync';
-import { getRandomInt, progressive } from '../src/index.js';
+import { getRandomInt, progressive } from '../index.js';
 
-console.log('Welcome to the Brain Games!');
-
-const name = readlineSync.question('May I have your name? ');
-console.log('Hello,', name, '!');
-
-console.log('What number is missing in the progression?');
-
-let i = 0;
-
-while (i < 3) {
-    let a1 = getRandomInt(15);
-    let d = getRandomInt(9);
-    let n = getRandomInt(10);
-    let x = 5;
-    if ( n < 5 ) {
-        x = getRandomInt(x);
-    } else {
-        x = getRandomInt(n);
-    };
-    
-    const mas = progressive(a1, d, n, x);
-
-    const name1 = readlineSync.question('Your answer: ');
-    const answer = parseInt(name1, 10);
-        if (answer === mas) {
-            i +=1;
-            console.log('Correct!');  
+const brProgression = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+    console.log('What number is missing in the progression?');
+    let i = 0;
+    while (i < 3) {
+        let a1 = getRandomInt(15);
+        let d = getRandomInt(9);
+        let n = getRandomInt(10);
+        let x = 5;
+        if ( n < 5 ) {
+            x = getRandomInt(x);
         } else {
-            i = 0;
-            console.log("'",name1,"' is wrong answer ;(. Correct answer was '",mas,"'.");
-            console.log("Let's try again,",name,'!');
-            break;
-        }; 
-
+            x = getRandomInt(n);
+        };
+        const mas = progressive(a1, d, n, x);
+        const name1 = readlineSync.question('Your answer: ');
+        const answer = parseInt(name1, 10);
+            if (answer === mas) {
+                i +=1;
+                console.log('Correct!');  
+            } else {
+                i = 0;
+                console.log(`'${name1}' is wrong answer ;(. Correct answer was '${mas}'.`);
+                console.log(`Let's try again, ${name}!`);
+                break;
+            }; 
+    };
+    if (i === 3) {
+        console.log(`Congratulations, ${name}!`);
+    };
 };
-
-if (i === 3) {
-    console.log(`Congratulations, ${name}!`);
-};
+export default brProgression;
